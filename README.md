9d8aa503415de5d7acddb9175c73ed1db5e664ad
ehdrn3020 <ehdrnsla3020@naver.com>
2024-12-16 23:05:49 +0900

    prepare project setting

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # kafka_system_with_azar
-카프카 시스템 구성, 아자르 비즈니스 메트릭 생성 프로세스 참조
+### 카프카 시스템 구성, 아자르 비즈니스 메트릭 생성 프로세스 참조
+<img src="image/architecture_azar.png" width="600">
---
+++ setting_aws/user_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# git clone 시에 root path에 실행하게 되므로 ec2-user home dirtory로 이동
+sudo yum install -y git && \
+sudo amazon-linux-extras install -y ansible2 && \
+cd /home/ec2-user && \
+sudo -u ec2-user git clone https://github.com/ehdrn3020/demo-kafka.git
